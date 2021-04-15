3fb4fb28d5bc940d4ff8142f0f807f5ac1bd20ec
darkmageman4 <darkmageman4@yahoo.com>
2021-04-15 17:06:38 -0400

    Shorthand

---
+++ JS/shorthand-unary-and-jsmethods.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//SHORTHAND ASSIGNMENT OPERATORS
+
+//+= / -=
+
+var name = "Casey";
+//
+// name = name + " Edwards";
+// console.log(name);
+
+name += " Edwards";
+console.log(name);
+
+//console.log(name -= "Edwards");
+
+// var dayRemainingInCohort = 114;
+//
+// dayRemainingInCohort -= 4;
+//
+// console.log(dayRemainingInCohort);
+
+// TODO: Refactor the mini exercise we did earlier on arithmetic operators to use variables and their related shorthand assignment operators, then log out the result
+//  ie:
+//     var result = 5;
+//     console.log(result += 1);
+
+var results = 5;
+console.log(results -=1);
+console.log(results *=1);
+console.log(results /=1);
+console.log(results +=0);
+

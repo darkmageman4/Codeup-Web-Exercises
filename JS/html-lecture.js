2c6e6ec3e305384fafaf41b4cd7f681f471eee63
darkmageman4 <darkmageman4@yahoo.com>
2021-05-03 14:34:42 -0400

    HTML

---
+++ JS/html-lecture.js
@@ -0,0 +1,12 @@
+(function (){
+
+   function stack(input) {
+      return input.trim()
+   }
+
+   function trim(input){
+      return input.replaceAll(' ','');
+   }
+   console.log(stack("   Girl Just Wanna Have Fun!  "))
+
+})()
--- extra-exercises/test.js
+++ extra-exercises/test.js
@@ -1,34 +1,189 @@
+(function() {
+    "use strict";
 
-// assert(forth("ubuntu"), "n", "Exercise 53");
-// assert(forth([1, 2, 3, 4]), 4, "Exercise 53");
-// assert(forth(["JS", "is", "awesome", "right?"]), "right?", "Exercise 53");
-// addToDone("Exercise 53 is correct.")
+    // function last(x){
+//     const lastItem = x[x.length - 1]
+//     return lastItem
+// }
 
 // function last (x){
 //     return x.slice(-1);
 // };
 //
-// console.log(last(["JS", "is", "awesome"]));
-
-
 // function secondToLast (x){
 //     return x[x.length - 2];
 // };
-// console.log(secondToLast(["JS", "is", "awesome"]));
-
-// function secondToLast (x){
-//     return x[x.length - 2];
-// };
-// console.log(secondToLast([1, 2, 3, 4]));
-
+//
 // function thirdToLast (x){
-//     return x[x.length - 1];
+//     return x[x.length - 3];
 // };
-// console.log(secondToLast([1, 2, 3, 4]));
-
+//
 // function firstAndSecond (x){
-//     return x.slice(0,3);
+//     return x.slice(0,2);
 // };
-// console.log(firstAndSecond([1, 2, 3, 4]));
+//
+//    function firstAndLast (x) {
+//         return [x[0], x[x.length - 1]];
+//     }
+//
+//     function firstToLast (x){
+//         var a = x.shift();
+//         x.push(a);
+//         return x
+//     }
+//     function sumAll(x) {
+//         var b = 0;
+//         x.forEach(function (a) {
+//             b += a;
+//         });
+//         return b;
+//     }
+
+    // function mean (x){
+    // var b = 0;
+    // x.forEach(function(a){
+    // b += a
+    // });
+    // return b / x.length
+    // }
+
+    // function median (x) {
+    //     var b = 0;
+    //     x.forEach(function (a) {
+    //         b += a
+    //     });
+    //     var average = b / x.length;
+    //     return average.toFixed(0)
+    // }
+
+    // function median(x){
+    //     if(x.length ===0) return 0;
+    //
+    //     x.sort(function(a,b){
+    //         return a-b;
+    //     });
+    //
+    //     var half = Math.floor(x.length / 2);
+    //
+    //     if (x.length % 2)
+    //         return x[half];
+    //
+    //     return (x[half - 1] + x[half]) / 2.0;
+    // }
 
 
+    const books = [
+        {
+            "title": "Genetic Algorithms and Machine Learning for Programmers",
+            "price": 36.99,
+            "author": "Frances Buontempo"
+        },
+        {
+            "title": "The Visual Display of Quantitative Information",
+            "price": 38.00,
+            "author": "Edward Tufte"
+        },
+        {
+            "title": "Practical Object-Oriented Design",
+            "author": "Sandi Metz",
+            "price": 30.47
+        },
+        {
+            "title": "Weapons of Math Destruction",
+            "author": "Cathy O'Neil",
+            "price": 17.44
+        }
+    ]
+
+    function totalOfBookPrices(x){
+        var a = 0;
+        for (var i = 0; i < x.length; i ++){
+            a += x[i].price;
+        } return a;
+    }
+
+
+    // function highestPriceBook(x) {
+    //     var a = {price: 0};
+    //
+    //     for (var x = 0; x < x.length; x++) {
+    //         if (x[x].price > a.price) {
+    //             a.price = x[x].price;
+    //         }
+    //     }return
+    // }
+    //
+    // function highestPriceBook(x) {
+    //     var spc = x.sort((a, b) => b.price - a.price);
+    //     return books[0]}
+    //
+    // function lowestPriceBook(x) {
+    //     return x.sort((a, b) => b.price - a.price);
+    // return books[3]}
+    //
+    const shoppingCart = {
+        "tax": .08,
+        "items": [
+            {
+                "title": "orange juice",
+                "price": 3.99,
+                "quantity": 1
+            },
+            {
+                "title": "rice",
+                "price": 1.99,
+                "quantity": 3
+            },
+            {
+                "title": "beans",
+                "price": 0.99,
+                "quantity": 3
+            },
+            {
+                "title": "chili sauce",
+                "price": 2.99,
+                "quantity": 1
+            },
+            {
+                "title": "chocolate",
+                "price": 0.75,
+                "quantity": 9
+            }
+        ]
+    }
+
+    // function getTaxRate (){
+    //     return shoppingCart.tax
+    // }
+    //
+    // function numberOfItemTypes (){
+    //     return Object.keys(shoppingCart.items).length
+    // }
+
+    // function totalNumberOfItems (){
+    //     return Object.keys(shoppingCart.items).length
+    // }
+
+    // function totalOfBookPrices(x){
+    //     var a = 0;
+    //     for (var i = 0; i < x.length; i ++){
+    //         a += x[i].price;
+    //     } return a;
+    // }
+    // console.log(totalOfBookPrices(books))
+
+
+
+
+
+    function totalNumberOfItems(x){
+        var a = 0;
+        for (var i = 0; i < x.length; i ++){
+            a += x[i].quantity;
+        }
+        return a;
+    }
+    console.log(totalNumberOfItems(shoppingCart))
+    console.log(totalNumberOfItems(shoppingCart.items))
+
+})();

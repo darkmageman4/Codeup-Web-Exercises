43bd0ac2f1c70e2e1badde192f14d6c58bb0ba4c
darkmageman4 <darkmageman4@yahoo.com>
2021-05-04 11:31:04 -0400

    Current files

--- extra-exercises/test.js
+++ extra-exercises/test.js
@@ -183,7 +183,19 @@
         }
         return a;
     }
-    console.log(totalNumberOfItems(shoppingCart))
-    console.log(totalNumberOfItems(shoppingCart.items))
+    // console.log(totalNumberOfItems(shoppingCart))
+    // console.log(totalNumberOfItems(shoppingCart.items))
 
 })();
+
+
+// Write a function named cube(n) that returns n * n * n
+// Write a function named cubeRoot(n) that returns the cube root of the input
+
+function cube(n){
+   return n * n * n
+}console.log(cube(3))
+
+function cubeRoot(n){
+    return Math.cbrt(n)
+}console.log(cubeRoot(216))

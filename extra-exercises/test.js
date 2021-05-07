064e05d7d398fda7d51cd3a5d214076631ff85d0
darkmageman4 <darkmageman4@yahoo.com>
2021-05-07 11:39:01 -0400

    CSS

--- extra-exercises/test.js
+++ extra-exercises/test.js
@@ -17,7 +17,6 @@
 // function thirdToLast (x){
 //     return x[x.length - 3];
 // };
-//
 // function firstAndSecond (x){
 //     return x.slice(0,2);
 // };
@@ -38,7 +37,6 @@
 //         });
 //         return b;
 //     }
-
     // function mean (x){
     // var b = 0;
     // x.forEach(function(a){
@@ -70,37 +68,34 @@
     //
     //     return (x[half - 1] + x[half]) / 2.0;
     // }
-
-
-    const books = [
-        {
-            "title": "Genetic Algorithms and Machine Learning for Programmers",
-            "price": 36.99,
-            "author": "Frances Buontempo"
-        },
-        {
-            "title": "The Visual Display of Quantitative Information",
-            "price": 38.00,
-            "author": "Edward Tufte"
-        },
-        {
-            "title": "Practical Object-Oriented Design",
-            "author": "Sandi Metz",
-            "price": 30.47
-        },
-        {
-            "title": "Weapons of Math Destruction",
-            "author": "Cathy O'Neil",
-            "price": 17.44
-        }
-    ]
-
-    function totalOfBookPrices(x){
-        var a = 0;
-        for (var i = 0; i < x.length; i ++){
-            a += x[i].price;
-        } return a;
-    }
+    // const books = [
+    //     {
+    //         "title": "Genetic Algorithms and Machine Learning for Programmers",
+    //         "price": 36.99,
+    //         "author": "Frances Buontempo"
+    //     },
+    //     {
+    //         "title": "The Visual Display of Quantitative Information",
+    //         "price": 38.00,
+    //         "author": "Edward Tufte"
+    //     },
+    //     {
+    //         "title": "Practical Object-Oriented Design",
+    //         "author": "Sandi Metz",
+    //         "price": 30.47
+    //     },
+    //     {
+    //         "title": "Weapons of Math Destruction",
+    //         "author": "Cathy O'Neil",
+    //         "price": 17.44
+    //     }
+    // ]
+    // function totalOfBookPrices(x){
+    //     var a = 0;
+    //     for (var i = 0; i < x.length; i ++){
+    //         a += x[i].price;
+    //     } return a;
+    // }
 
 
     // function highestPriceBook(x) {
@@ -121,37 +116,36 @@
     //     return x.sort((a, b) => b.price - a.price);
     // return books[3]}
     //
-    const shoppingCart = {
-        "tax": .08,
-        "items": [
-            {
-                "title": "orange juice",
-                "price": 3.99,
-                "quantity": 1
-            },
-            {
-                "title": "rice",
-                "price": 1.99,
-                "quantity": 3
-            },
-            {
-                "title": "beans",
-                "price": 0.99,
-                "quantity": 3
-            },
-            {
-                "title": "chili sauce",
-                "price": 2.99,
-                "quantity": 1
-            },
-            {
-                "title": "chocolate",
-                "price": 0.75,
-                "quantity": 9
-            }
-        ]
-    }
-
+    // const shoppingCart = {
+    //     "tax": .08,
+    //     "items": [
+    //         {
+    //             "title": "orange juice",
+    //             "price": 3.99,
+    //             "quantity": 1
+    //         },
+    //         {
+    //             "title": "rice",
+    //             "price": 1.99,
+    //             "quantity": 3
+    //         },
+    //         {
+    //             "title": "beans",
+    //             "price": 0.99,
+    //             "quantity": 3
+    //         },
+    //         {
+    //             "title": "chili sauce",
+    //             "price": 2.99,
+    //             "quantity": 1
+    //         },
+    //         {
+    //             "title": "chocolate",
+    //             "price": 0.75,
+    //             "quantity": 9
+    //         }
+    //     ]
+    // }
     // function getTaxRate (){
     //     return shoppingCart.tax
     // }
@@ -171,31 +165,35 @@
     //     } return a;
     // }
     // console.log(totalOfBookPrices(books))
-
-
-
-
-
-    function totalNumberOfItems(x){
-        var a = 0;
-        for (var i = 0; i < x.length; i ++){
-            a += x[i].quantity;
-        }
-        return a;
-    }
+    // function totalNumberOfItems(x){
+    //     var a = 0;
+    //     for (var i = 0; i < x.length; i ++){
+    //         a += x[i].quantity;
+    //     }
+    //     return a;
+    // }
     // console.log(totalNumberOfItems(shoppingCart))
     // console.log(totalNumberOfItems(shoppingCart.items))
+// Write a function named cube(n) that returns n * n * n
+// Write a function named cubeRoot(n) that returns the cube root of the input
+// function cube(n){
+//    return n * n * n
+// }console.log(cube(3))
+// function cubeRoot(n){
+//     return Math.cbrt(n)
+// }console.log(cubeRoot(216))
+// function remainder (number,divisor) {
+//     var checkA = parseInt(number)
+//     var checkB = parseInt(divisor)
+//     var answer = checkA % checkB
+//     if (!isNaN(answer)) {
+//         return answer
+//     } else
+//         return false
+// }
+
+
+
 
 })();
 
-
-// Write a function named cube(n) that returns n * n * n
-// Write a function named cubeRoot(n) that returns the cube root of the input
-
-function cube(n){
-   return n * n * n
-}console.log(cube(3))
-
-function cubeRoot(n){
-    return Math.cbrt(n)
-}console.log(cubeRoot(216))

cee27b580a6f09603d0daee703704ea1400051fb
darkmageman4 <darkmageman4@yahoo.com>
2021-05-20 14:38:54 -0400

    dom lecture exercises

---
+++ JS/dom-practice.js
@@ -0,0 +1,16 @@
+// TODO: change the text inside of 1 of the paragraphs to be a short paragraph about you-->
+var iChanged = document.getElementsByTagName(`p`);
+console.log(iChanged.innerText);
+iChanged[0].innerText = "I am the coolest guy in the world"
+console.log(iChanged.innerText)
+
+
+
+
+
+
+
+ // TODO: log the second paragraph element from the list of all-->
+// TODO: create a new ul element and 5 new li elements. Each of your li's should have some text inside of it-->
+// TODO: replace the last div's paragraph with your new ul. make sure the ul has all 5 li's inside of it-->
+ // TODO: remove the last li-->

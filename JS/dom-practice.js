3136d8fb97b6a28a8df8bf00e807a81e2d74808a
darkmageman4 <darkmageman4@yahoo.com>
2021-05-20 16:51:04 -0400

    dom-extra exercises

--- JS/dom-practice.js
+++ JS/dom-practice.js
@@ -3,14 +3,13 @@ var iChanged = document.getElementsByTagName(`p`);
 console.log(iChanged.innerText);
 iChanged[0].innerText = "I am the coolest guy in the world"
 console.log(iChanged.innerText)
-
-
-
-
-
-
-
  // TODO: log the second paragraph element from the list of all-->
+console.log(iChanged);
 // TODO: create a new ul element and 5 new li elements. Each of your li's should have some text inside of it-->
 // TODO: replace the last div's paragraph with your new ul. make sure the ul has all 5 li's inside of it-->
  // TODO: remove the last li-->
+
+
+
+
+

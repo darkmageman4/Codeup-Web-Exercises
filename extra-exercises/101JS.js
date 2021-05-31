4950a23f5efbce49862c6b5fa58737ec0925a55c
darkmageman4 <darkmageman4@yahoo.com>
2021-05-31 17:41:04 -0400

    Into to JQuery

--- extra-exercises/101JS.js
+++ extra-exercises/101JS.js
@@ -1,3 +1,6 @@
+// (function () {
+
+
 const shoppingCart = {
     tax: .08,
     items: [
@@ -39,23 +42,22 @@ function getAverageItemPrice1(shoppingCart) {
 // console.log(getAverageItemPrice1(shoppingCart))
 
 
-
-function getAverageItemPrice() {
-    let shop = [shoppingCart.items[0].price,
-        shoppingCart.items[1].price,
-        shoppingCart.items[2].price,
-        shoppingCart.items[3].price,
-        shoppingCart.items[4].price];
-
-    let avg = 0;
-
-    for (let i = 0; i < shop.length; i++) {
-        avg += shop[i]/5;
-    }
-    return avg
-}
-
-console.log(getAverageItemPrice(shoppingCart))
+// function getAverageItemPrice() {
+//     let shop = [shoppingCart.items[0].price,
+//         shoppingCart.items[1].price,
+//         shoppingCart.items[2].price,
+//         shoppingCart.items[3].price,
+//         shoppingCart.items[4].price];
+//
+//     let avg = 0;
+//
+//     for (let i = 0; i < shop.length; i++) {
+//         avg += shop[i] / 5;
+//     }
+//     return avg
+// }
+//
+// console.log(getAverageItemPrice(shoppingCart))
 
 // let shoppingCart = [{
 //     product: 'phone',
@@ -75,11 +77,42 @@ console.log(getAverageItemPrice(shoppingCart))
 // ];
 
 let total = shoppingCart.items.reduce(function (acc, curr) {
-    return acc + curr.quantity * curr.price /5;
-},0);
+    return acc + curr.quantity * curr.price / 5;
+}, 0);
 
 // console.log(total);
+//TODO: // Write a function named getAverageSpentPerItem that takes in the shopping cart and returns the average of summing each item's quanties times that item's
+// price. Hint: You may need to set an initial total price and total total quantity to zero, then sum up and divide that total price by the total quantity
 
-function  getAverageSpentPerItem(){
 
+function getAverageSpentPerItem(cart) {
+    var totalPrice = 0;
+    var totalQuantity = 0;
+    for (var i = 0; i < shoppingCart.items.length; i++) {
+        totalPrice =
+            totalPrice + shoppingCart.items[i].price * shoppingCart.items[i].quantity;
+        totalQuantity = totalQuantity + shoppingCart.items[i].quantity;
+    }
+    return totalPrice / totalQuantity;
 }
+
+console.log(getAverageSpentPerItem(shoppingCart))
+// });
+
+function mostSpentOnItem() {
+    var compareProductOfItem = 0;
+    var indexOf = 0;
+    for (var i = 0; i < shoppingCart.items.length; i++) {
+        if (
+            shoppingCart.items[i].price * shoppingCart.items[i].quantity >
+            compareProductOfItem
+        ) {
+            compareProductOfItem =
+                shoppingCart.items[i].price * shoppingCart.items[i].quantity;
+            indexOf = i;
+        }
+    }
+    return shoppingCart.items[indexOf];
+}
+
+console.log(mostSpentOnItem(shoppingCart))

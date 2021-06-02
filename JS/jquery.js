eb1425c7348cb590be278240e7fdc2c4f89214ba
darkmageman4 <darkmageman4@yahoo.com>
2021-06-02 13:05:35 -0400

    Konami code

--- JS/jquery.js
+++ JS/jquery.js
@@ -23,27 +23,34 @@
 // $("h1"[,"p"[,"li"]])
 
 // $(document).ready(function (){
-    // $("h1","p","li").css('background-color', '#FFC0CB',"1px");
+//     $("h1,p,li").css('background-color', '#FFC0CB',"1px");
 // });
 
-$("h1").click(function (){
-    alert("STOP CLICKING ME!!!!");
-    $(this).css('background-color', '#FF0');
-});
+// $("h1").click(function (){
+//     alert("STOP CLICKING ME!!!!");
+//     $(this).css('background-color', '#FF0');
+// });
+//
+// $("p").dblclick(function(){
+//     alert("NOW LOOK AT WHAT YOU'VE DONE");
+//     $(this).css("font-size","18px");
+// });
+//
+// $("li").hover(
+//     function (){
+//     // alert("AHHHH!!!!!!");
+//     $(this).css('color', 'red');
+// },
+//     function(){
+//         $(this).css('color', 'white');
+//     });
 
-$("p").dblclick(function(){
-    alert("NOW LOOK AT WHAT YOU'VE DONE");
-    $(this).css("font-size","18px");
-});
 
-$("li").hover(
-    function (){
-    // alert("AHHHH!!!!!!");
-    $(this).css('color', 'red');
-},
-    function(){
-        $(this).css('color', 'white');
-    });
+var konamikeys = [38,38,40,40,37,39,37,39,66,65],
+
+    $(document).keyup(function(event){
+    console.log(event.keyCode);
+});
 
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );

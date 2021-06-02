a2655d4ab3828fff242a64c6da3568061fe3104e
darkmageman4 <darkmageman4@yahoo.com>
2021-06-02 10:06:52 -0400

    Jquerey exercise

--- JS/jquery.js
+++ JS/jquery.js
@@ -4,32 +4,46 @@
 //     alert($("#growOnTrees").text());
 // });
 
-
-
 // $(document).ready(function() {
 //     alert( 'The DOM has finished loading!' );
 // });
-//
+
 // $(".codeup").css('background-color', '#FFC0CB',"1px");
-
-
 
 // $("li").css('font-size', "20px");
 
 // $(document).ready(function(){
 //     alert($("h1").text())
-// })
+// });
 
 // $(document).ready(function(){
 //     alert($("h1").html())
-// })
+// });
 
 // $("h1"[,"p"[,"li"]])
 
 // $(document).ready(function (){
-    $("h1","p","li").css('background-color', '#FFC0CB',"1px");
+    // $("h1","p","li").css('background-color', '#FFC0CB',"1px");
+// });
 
-// })
+$("h1").click(function (){
+    alert("STOP CLICKING ME!!!!");
+    $(this).css('background-color', '#FF0');
+});
+
+$("p").dblclick(function(){
+    alert("NOW LOOK AT WHAT YOU'VE DONE");
+    $(this).css("font-size","18px");
+});
+
+$("li").hover(
+    function (){
+    // alert("AHHHH!!!!!!");
+    $(this).css('color', 'red');
+},
+    function(){
+        $(this).css('color', 'white');
+    });
 
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );

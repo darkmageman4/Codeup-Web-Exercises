fd00782a307d688c3e7700a099b5f6939fbb1778
darkmageman4 <darkmageman4@yahoo.com>
2021-06-16 10:07:36 -0400

    more dom exercise

--- JS/casey-exercise.js
+++ JS/casey-exercise.js
@@ -1,62 +1,62 @@
 (function () {
-    let jqElements = $('.jqClass');
-    let backgroundBlue = 'lightblue';
-    let backgroundReset = '';
-
-    function addClickEvents(elementsToBind, color1, color2) {
-        let handlerBlueBckGnd = function () {
-            elementsToBind.css('background-color', color1);
-            $(this).css('background-color', color2);
-        }
-        elementsToBind.click(handlerBlueBckGnd);
-    }
-
-    addClickEvents(jqElements, backgroundReset, backgroundBlue);
-    addClickEvents($(".card"), "", "red");
-    // make sure the DOM is loaded first
-    document.addEventListener("DOMContentLoaded", function () {
-        addListeners();
-    });
-    // $(document).ready(function(addListeners);
-
-    //parent method for adding all listeners
-    function addListeners() {
-        //get the elements to add listener and change image
-        let cardContainers = Array.from(document.querySelectorAll(".col-md-3"));
-        addEvents(cardContainers, 'kodak/jazz-music-rubber-duck.jpg', 'mouseover');
-        addEvents(cardContainers, 'kodak/300x300.png', 'mouseout');
-
-        // $( "#greatphoto" ).attr( "alt", "Beijing Brush Seller" );
-//         let cardContainers = $(".col-md-3");
-// addEvents(cardContainers,"kodak/jazz-music-rubber-duck.jpg","kodak/jazz-music-rubber-duck.jpg")
-//     )
-        //     $('.col-md-3').hover(
-        //         function () {
-        //             $(this).mouseenter( "kodak/jazz-music-rubber-duck.jpg");
-        //         },
-        //         function () {
-        //             $(this).mouseleave("kodak/question.png");
-        //         }
-        //     );
-    }
-
-    //add mouseover/mouseout events to all selected elements
-    function addEvents(cardContainers, imgPath, listenerType) {
-        cardContainers.forEach((cc) => {
-            let card = cc;
-            //create listener which will call function change the image
-            let listener = function (event) {
-                let cardImg = card.querySelector(".card .card-img-top");
-                changeImage(cardImg, imgPath);
-            };
-            cc.addEventListener(listenerType, listener);
-        });
-    }
-
-    //actual function to swap images
-    function changeImage(card, imgPath) {
-        card.setAttribute('src', imgPath);
-    }
+//     let jqElements = $('.jqClass');
+//     let backgroundBlue = 'lightblue';
+//     let backgroundReset = '';
+//
+//     function addClickEvents(elementsToBind, color1, color2) {
+//         let handlerBlueBckGnd = function () {
+//             elementsToBind.css('background-color', color1);
+//             $(this).css('background-color', color2);
+//         }
+//         elementsToBind.click(handlerBlueBckGnd);
+//     }
+//
+//     addClickEvents(jqElements, backgroundReset, backgroundBlue);
+//     addClickEvents($(".card"), "", "red");
+//     // make sure the DOM is loaded first
+//     document.addEventListener("DOMContentLoaded", function () {
+//         addListeners();
+//     });
+//     // $(document).ready(function(addListeners);
+//
+//     //parent method for adding all listeners
+//     function addListeners() {
+//         //get the elements to add listener and change image
+//         let cardContainers = Array.from(document.querySelectorAll(".col-md-3"));
+//         addEvents(cardContainers, 'kodak/jazz-music-rubber-duck.jpg', 'mouseover');
+//         addEvents(cardContainers, 'kodak/300x300.png', 'mouseout');
+//
+//         // $( "#greatphoto" ).attr( "alt", "Beijing Brush Seller" );
+// //         let cardContainers = $(".col-md-3");
+// // addEvents(cardContainers,"kodak/jazz-music-rubber-duck.jpg","kodak/jazz-music-rubber-duck.jpg")
+// //     )
+//         //     $('.col-md-3').hover(
+//         //         function () {
+//         //             $(this).mouseenter( "kodak/jazz-music-rubber-duck.jpg");
+//         //         },
+//         //         function () {
+//         //             $(this).mouseleave("kodak/question.png");
+//         //         }
+//         //     );
+//     }
+//
+//     //add mouseover/mouseout events to all selected elements
+//     function addEvents(cardContainers, imgPath, listenerType) {
+//         cardContainers.forEach((cc) => {
+//             let card = cc;
+//             //create listener which will call function change the image
+//             let listener = function (event) {
+//                 let cardImg = card.querySelector(".card .card-img-top");
+//                 changeImage(cardImg, imgPath);
+//             };
+//             cc.addEventListener(listenerType, listener);
+//         });
+//     }
+//
+//     //actual function to swap images
+//     function changeImage(card, imgPath) {
+//         card.setAttribute('src', imgPath);
+//     }
 
 
     // TODO: When the user clicks the button with the id of 'submitBtn,' the background color of #override-bootstrap changes to 'red'
@@ -150,5 +150,27 @@
 //             console.log(persons)
 //         })
 //     }
+
+
+
+    $("h2").hover(function (){
+        $("h2").css("color","red");
+    }, function (){
+        $("h2").css("color","");
+    })
+
+    // $(".card").click(function(){
+    //     $(this).css("border", "3px double red" )
+    // })
+
+    $("#submitBtn").click(function(){
+        window.location.replace("https://codeup.com/");
+    })
+
+    $(".col-md-3").dblclick(function(){
+        let daBat =  $(this).attr("data-attribute")
+        $(this).find(".card-text").text(daBat);
+    })
+
 
 })();
